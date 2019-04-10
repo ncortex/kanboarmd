7cb89d254af61a2a1c6a66b4daf2500855b3c641
linares <jose.linares.1993@gmail.com>
2019-04-10 17:36:21 +0200

    plugin filters

--- plugins/MetaMagik/Template/task/rendermeta1.php
+++
@@ -1,1 +0,0 @@
- <?= $this->helper->metaHelper->renderMetaFields($values, 1, $errors) ?>
--- plugins/MetaMagik/Template/user/sidebar.php
+++
@@ -1,7 +0,0 @@
-<?php if ($this->user->isAdmin()) {
-    ?>
-    <li>
-        <?= $this->url->link(t('Metadata'), 'MetadataController', 'user', ['plugin' => 'metaMagik', 'user_id' => $user['id']]) ?>
-    </li>
-<?php 
-} ?>
