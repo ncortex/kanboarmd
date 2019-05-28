e05dcb446fb73620dd8aab62e44589687095d5c9
linares <jose.linares.1993@gmail.com>
2019-05-28 12:23:23 +0200

    quityando botoned de crear tarea

--- plugins/MinimalistTheme/Template/proyect_header/new_task.php
+++ plugins/MinimalistTheme/Template/proyect_header/new_task.php
@@ -1,6 +1,19 @@
 <?php
 
 if ($this->projectRole->canCreateTaskInColumn(1, 5)) {
-    $this->task->getNewBoardTaskButton([0], 5);
+    $html = '<div class="board-add-icon">';
+    $html .= $this->helper->modal->largeIcon(
+        'plus',
+        t('Add a new task'),
+        'TaskCreationController',
+        'show', array(
+            'project_id'  => 1,
+            'column_id'   => 5,
+            'swimlane_id' => 1,
+        )
+    );
+    $html .= '</div>';
+
+    return $html;
 }
 ?>
