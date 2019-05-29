cb05145d589ec21d51edd96657f92c9e4d655b84
linares <jose.linares.1993@gmail.com>
2019-05-29 13:12:21 +0200

    mejorando fordchain

--- plugins/Fordchain/Template/task/taskEditold.php
+++ plugins/Fordchain/Template/task/taskEditold.php
@@ -1,5 +1,5 @@
 <div class="page-header">
-    <h2><?= $this->text->e($project['name']) ?> &gt; <?= t('New task') ?></h2>
+    <h2><?= $this->text->e($project['name']) ?> &gt; <?= $this->text->e($task['title']) ?></h2>
 </div>
 <form method="post" id="formTaskCreation" action="<?= $this->url->href('TaskModificationController', 'update', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>" autocomplete="off">
     <?= $this->form->csrf() ?>
@@ -7,20 +7,20 @@
     <div class="task-form-container">
         <div class="task-form-main-column">
             <?= $this->task->renderTitleField($values, $errors) ?>
-            <!--= $this->task->renderDescriptionField($values, $errors) -->
-            <?= $this->hook->render('template:task:form:first-column', array('values' => $values, 'errors' => $errors)) ?>
+            <?= $this->task->renderDescriptionField($values, $errors) ?>
             <?= $this->task->renderCategoryField($categories_list, $values, $errors) ?>
             <?= $this->task->renderTagField($project) ?>
 
+            <?= $this->hook->render('template:task:form:first-column', array('values' => $values, 'errors' => $errors)) ?>
         </div>
 
         <div class="task-form-secondary-column">
-            <div style="display: none"> <?= $this->helper->fordchainHelper->renderFordAssigneeField($users_list, $values, $errors, array(), "owner_id") ?> </div>
-            <?= $this->helper->fordchainHelper->renderFordAssigneeField($users_list, $values, $errors, [], "gestor_id", "Project Manager") ?>
-            <?= $this->helper->fordchainHelper->renderFordAssigneeField($users_list, $values, $errors, [], "translator_id", "Traductor") ?>
-            <?= $this->helper->fordchainHelper->renderFordAssigneeField($users_list, $values, $errors, [], "reviewer_id", "Revisor") ?>
+            <div style="display: none"> <?= $this->task->renderAssigneeField($users_list, $values, $errors, array(), "owner_id") ?> </div>
+            <?= $this->task->renderAssigneeField($users_list, $values, $errors, ['disabled="disabled"'], "gestor_id", "Project Manager") ?>
+            <?= $this->task->renderAssigneeField($users_list, $values, $errors, [], "translator_id", "Traductor") ?>
+            <?= $this->task->renderAssigneeField($users_list, $values, $errors, [], "reviewer_id", "Revisor") ?>
 
-            <?= $this->hook->render('template:task:form:second-column', array('values' => $values, 'errors' => $errors, 'users_list' => $users_list)) ?>
+            <?= $this->hook->render('template:task:form:second-column', array('values' => $values, 'errors' => $errors)) ?>
         </div>
 
         <div class="task-form-secondary-column">
@@ -30,11 +30,9 @@
             <?= $this->task->renderColorField($values) ?>
 
             <?= $this->hook->render('template:task:form:third-column', array('values' => $values, 'errors' => $errors)) ?>
-
         </div>
 
         <div class="task-form-bottom">
-
             <?= $this->modal->submitButtons() ?>
         </div>
     </div>
