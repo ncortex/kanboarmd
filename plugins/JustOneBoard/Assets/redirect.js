92cabbbf1e55008d9aea13be58bb1fecd623c3c6
linares <jose.linares.1993@gmail.com>
2019-05-30 18:44:48 +0200

    redireccion patata

--- plugins/JustOneBoard/Assets/redirect.js
+++ plugins/JustOneBoard/Assets/redirect.js
@@ -1,4 +1,4 @@
 console.log(window.location);
 if( window.location == "https://kanboard-4.herokuapp.com/") {
-    window.location.replace('board/1');
+    window.location.replace("https://kanboard-4.herokuapp.com/?controller=BoardViewController&action=show&project_id=1");
 }
