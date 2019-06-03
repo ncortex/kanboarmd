4c561131217a2536f32f6358d9ba8e717e08fe28
linares <jose.linares.1993@gmail.com>
2019-06-03 15:09:14 +0200

    probando ajax

--- plugins/Fordchain/Template/task/taskEdit.php
+++ plugins/Fordchain/Template/task/taskEdit.php
@@ -14,7 +14,7 @@
                     <?= $this->task->renderCategoryField($categories_list, $values, $errors) ?>
                 </div>
                 <div class="extra_field">
-                    <?= $this->task->renderTagField($project) ?>
+                    <?= $this->task->renderTagField($project) ; var_dump($values) ?>
                 </div>
             </div>
 
