679bb528298111c371693f5fbae00718d1e813ef
linares <jose.linares.1993@gmail.com>
2019-06-03 12:11:20 +0200

    añadiendo funcion ajax al js

--- plugins/TitleCompose/Assets/js/dependent-select.js
+++ plugins/TitleCompose/Assets/js/dependent-select.js
@@ -40,6 +40,19 @@ function clientChange(){
     }
 }
 
+function callAjax(url, callback){
+    var xmlhttp;
+    // compatible with IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            callback(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
 function productChange(){
     subproductSel.length = 1; // remove all options bar first
     if (this.selectedIndex < 1) {
