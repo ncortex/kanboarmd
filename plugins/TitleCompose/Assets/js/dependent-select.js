05418343376b7837c3e53d70adca454219bc8b59
linares <jose.linares.1993@gmail.com>
2019-06-03 15:19:41 +0200

    probando ajax

--- plugins/TitleCompose/Assets/js/dependent-select.js
+++ plugins/TitleCompose/Assets/js/dependent-select.js
@@ -42,7 +42,7 @@ function clientChange(json_productos){
 
 function productChange(json_subproductos){
     var subproductSel = document.getElementById("form-subproduct_id");
-    subproductSel.length = 1; // remove all options bar first
+    subproductSel.length = 0; // remove all options bar first
     //subproductSel.options[0].text = "Select subproduct";
 
     JSON.parse(json_subproductos).forEach(function(valor, indice, array) {
