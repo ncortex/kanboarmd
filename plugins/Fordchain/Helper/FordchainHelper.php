1d945130c7bb167b2851857387ac00bf1cad9fc3
linares <jose.linares.1993@gmail.com>
2019-06-04 11:21:01 +0200

    probando para cambiar el comportamiento del boton de mover

--- plugins/Fordchain/Helper/FordchainHelper.php
+++ plugins/Fordchain/Helper/FordchainHelper.php
@@ -10,13 +10,17 @@ use Kanboard\Core\Base;
  */
 class FordchainHelper extends Base
 {
-    public function renderFordAssigneeField(array $users, array $values, array $errors = array(), array $attributes = array(), $name='owner_id', $label="Assignee")
+    public function renderFordAssigneeField(array $users, array $values, array $errors = array(), array $attributes = array(), $name='owner_id', $label="Assignee", $required=false)
     {
             if (isset($values['project_id']) && ! $this->helper->projectRole->canChangeAssignee($values)) {
                 return '';
             }
 
             $attributes = array_merge(array('tabindex="3"'), $attributes);
+
+            if($required){
+                $attributes = array_merge(array('required=""'), $attributes);
+            }
 
             $html = $this->helper->form->label(t($label), $name);
             $html .= $this->helper->form->select($name, $users, $values, $errors, $attributes);
