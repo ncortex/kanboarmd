355a1f9379b8590e5410957fe2f8623f5d86a5b4
linares <jose.linares.1993@gmail.com>
2019-06-04 18:43:45 +0200

    cerrando tarea como ultimo paso del fordchain

--- plugins/Fordchain/Template/task/linkToNextStep.php
+++ plugins/Fordchain/Template/task/linkToNextStep.php
@@ -1,4 +1,4 @@
-<? if ($task['owner_id'] == $this->user->getId() && $task['column_id'] != 5): ?>
+<? if ($task['owner_id'] == $this->user->getId() && $task['column_id'] == 7): ?>
     <? echo($this->url->link('<button class="btn btn-red" style="float: right;">&gtClose&lt</button>', 'FordchainController', 'nextChainStep', array('plugin' => 'Fordchain', 'task_id' => $task['id'], 'project_id' => $task['project_id']),false,"js-modal-medium","",false));?>
 <? elseif ($task['owner_id'] == $this->user->getId() && $task['column_id'] != 5): ?>
     <? echo($this->url->link('<button class="btn btn-green" style="float: right;">Move &gt&gt</button>', 'FordchainController', 'nextChainStep', array('plugin' => 'Fordchain', 'task_id' => $task['id'], 'project_id' => $task['project_id']),false,"js-modal-medium","",false));?>
