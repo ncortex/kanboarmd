8c412246be6a9df75ba0fafcf77bab9c4b552eb0
linares <jose.linares.1993@gmail.com>
2019-06-04 18:45:05 +0200

    cerrando tarea como ultimo paso del fordchain

--- plugins/Fordchain/Template/task/linkToNextStep.php
+++ plugins/Fordchain/Template/task/linkToNextStep.php
@@ -1,5 +1,5 @@
-<? if ($task['owner_id'] == $this->user->getId() && $task['column_id'] == 7): ?>
+<? if ($task['owner_id'] == $this->user->getId() && $task['column_id'] == 8): ?>
     <? echo($this->url->link('<button class="btn btn-red" style="float: right;">&gtClose&lt</button>', 'FordchainController', 'nextChainStep', array('plugin' => 'Fordchain', 'task_id' => $task['id'], 'project_id' => $task['project_id']),false,"js-modal-medium","",false));?>
-<? elseif ($task['owner_id'] == $this->user->getId() && $task['column_id'] != 5): ?>
+<? elseif ($task['owner_id'] == $this->user->getId() && $task['column_id'] != 5 && $task['column_id'] != 9): ?>
     <? echo($this->url->link('<button class="btn btn-green" style="float: right;">Move &gt&gt</button>', 'FordchainController', 'nextChainStep', array('plugin' => 'Fordchain', 'task_id' => $task['id'], 'project_id' => $task['project_id']),false,"js-modal-medium","",false));?>
 <? endif ?>
