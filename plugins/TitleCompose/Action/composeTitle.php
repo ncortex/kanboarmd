2106eefeaa9a17a47fc522babc6cbc4aba6ecfb1
linares <jose.linares.1993@gmail.com>
2019-06-04 12:22:37 +0200

    probando cambios

--- plugins/TitleCompose/Action/composeTitle.php
+++ plugins/TitleCompose/Action/composeTitle.php
@@ -80,10 +80,12 @@ class composeTitle extends Base
         }
         // Los de rmsoft lo quieren con este formato exacto:
         $title = $subprodname . " - " . $data['task']['project_number'] . " (" . $data['task']['package_number'] .")";
+        $description = "".$this->taskMetadataModel->getAll($data['task']['id'])['mt'];
 
         $values = array(
             'id' => $data['task']['id'],
             'title' => $title,
+            'description' => $description
         );
 
         return $this->taskModificationModel->update($values, false);
