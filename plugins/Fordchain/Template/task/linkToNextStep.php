acab2ac88e71d0da09d56fb64310c51da229b4ce
linares <jose.linares.1993@gmail.com>
2019-06-04 10:33:25 +0200

    probando para cambiar el comportamiento del boton de mover

--- plugins/Fordchain/Template/task/linkToNextStep.php
+++ plugins/Fordchain/Template/task/linkToNextStep.php
@@ -1,3 +1,3 @@
 <? if ($task['owner_id'] == $this->user->getId()): ?>
-    <? echo($this->url->link('Move >>', 'FordchainController', 'nextChainStep', array('plugin' => 'Fordchain', 'task_id' => $task['id'], 'project_id' => $task['project_id']),false,"js-modal-medium","",false));?>
-<? endif ?>k
+    <? echo($this->url->link('<button class="btn btn-green">Move &gt&gt</button>', 'FordchainController', 'nextChainStep', array('plugin' => 'Fordchain', 'task_id' => $task['id'], 'project_id' => $task['project_id']),false,"js-modal-medium","",false));?>
+<? endif ?>
