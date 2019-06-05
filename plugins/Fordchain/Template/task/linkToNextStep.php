a45e4d9ef4045b13f9007988d5c7600e22f40e80
linares <jose.linares.1993@gmail.com>
2019-06-05 18:10:16 +0200

    probando

--- plugins/Fordchain/Template/task/linkToNextStep.php
+++ plugins/Fordchain/Template/task/linkToNextStep.php
@@ -1,5 +1,5 @@
 <? if ($task['owner_id'] == $this->user->getId() && $task['column_id'] == 8): ?>
-    <div class="btn btn-red" style="float: right;"><?= $this->modal->small('','>Close<', 'FordchainController', 'nextChainStepConfirm', array('plugin' => 'Fordchain', 'task_id' => $task['id'], 'project_id' => $task['project_id']),false,"popover");?></div>
+    <div class="btn btn-red" style="float: right;"><?= $this->modal->small('close','>Close', 'FordchainController', 'nextChainStepConfirm', array('plugin' => 'Fordchain', 'task_id' => $task['id'], 'project_id' => $task['project_id']),false,"popover");?></div>
 <? elseif ($task['owner_id'] == $this->user->getId() && $task['column_id'] != 5 && $task['column_id'] != 9): ?>
     <div class="btn btn-green" style="float: right;"><?= $this->modal->small('','Move >>', 'FordchainController', 'nextChainStepConfirm', array('plugin' => 'Fordchain', 'task_id' => $task['id'], 'project_id' => $task['project_id']),false,"popover");?></div>
 <? endif ?>
