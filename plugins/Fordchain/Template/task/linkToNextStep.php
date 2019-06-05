a2bdfe5a38e65ec92f886df965cc5b96dfed5a6c
linares <jose.linares.1993@gmail.com>
2019-06-05 13:34:11 +0200

    haciendo modal el avance de cadena

--- plugins/Fordchain/Template/task/linkToNextStep.php
+++ plugins/Fordchain/Template/task/linkToNextStep.php
@@ -1,6 +1,6 @@
 <? if ($task['owner_id'] == $this->user->getId() && $task['column_id'] == 8): ?>
     <? echo($this->url->link('<button class="btn btn-red" style="float: right;">&gtClose&lt</button>', 'FordchainController', 'nextChainStep', array('plugin' => 'Fordchain', 'task_id' => $task['id'], 'project_id' => $task['project_id']),false,"js-modal-medium","",false));?>
 <? elseif ($task['owner_id'] == $this->user->getId() && $task['column_id'] != 5 && $task['column_id'] != 9): ?>
-    <?= $this->modal->small('move','<div class="btn btn-red" style="float: right;">Move&gt&gt</div>', 'FordchainController', 'nextChainStepConfirm', array('plugin' => 'Fordchain', 'task_id' => $task['id'], 'project_id' => $task['project_id']),false,"popover");?>
+    <div class="btn btn-red" style="float: right;"><?= $this->modal->small('move','Move >>', 'FordchainController', 'nextChainStepConfirm', array('plugin' => 'Fordchain', 'task_id' => $task['id'], 'project_id' => $task['project_id']),false,"popover");?></div>
 <? endif ?>
 
